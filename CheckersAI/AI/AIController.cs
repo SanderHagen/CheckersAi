97b760eefe0d48295cdab1700e3a1943131987d1
sander hagen <sanderhagen@outlook.com>
2020-08-07 19:52:03 +0200

    start aicontroller

---
+++ CheckersAI/AI/AIController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CheckersAI.Game;
+
+namespace CheckersAI.AI
+{
+    class AIController
+    {
+        private Board board;
+        public event EventHandler AiMoveDone;
+
+        public AIController(Board board)
+        {
+            this.board = board;
+        }
+
+        public void DoMove()
+        {
+
+            return;
+        }
+    }
+}
